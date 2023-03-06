3a42e3cf500ca0f83b6aa6c48aa1da623c9b605b
jcleme <clemensjosh@protonmail.com>
2023-03-06 18:41:01 -0500

    testing websocket conection

---
+++ src/routes/testing/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	// const axios = require('axios').default;
+	import {} from '@sveltejs/kit';
+	import { browser, building, dev, version } from '$app/environment';
+
+	// let messageFromServer: string;
+
+	let messagesFromServer: string[];
+	messagesFromServer.push('');
+
+	if (browser) {
+		// let connection = new WebSocket('ws://100.83.232.113:3883/socket', ['soap', 'xmpp']);
+		let connection = new WebSocket('ws://hcd-lab.student.rit.edu:3883/socket', ['soap', 'xmpp']);
+		connection.onmessage = function (message: MessageEvent) {
+			// messageFromServer = message.data;
+			messagesFromServer.push(message.data);
+		};
+	}
+</script>
+
+<div>
+	{#each messagesFromServer as message}
+		<p>{message}</p>
+	{/each}
+</div>
