34a3a5565538f5f90de44ff3a64484a28f7faffc
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-19 22:02:22 -0400

    Added first phase page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,68 @@ function set_BG(){
   return "#"+randomColor;
 }
 </script>
-<div class="border border-black w-[300px] p-2 outer">
-    <div>
-        <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
-            <h1 class="text-4xl text-white">Movement Phase</h1>
+
+<div class="flex flex-col justify-center w-full items-center p-4">
+    <div class="border border-black w-[300px] p-2 outer">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">Movement Phase</h1>
+            </div>
         </div>
     </div>
-</div>
-
-<div class="border border-black w-[300px] p-2 outer">
-    <div class="">
-        <div class=" box border border-black flex justify-center p-1 w-fit px-2" style="background-color: {color} ;">
-            <h1 class="text-4xl text-white">100</h1>
+    
+    <div class="border border-black w-[300px] p-2 outer flex flex-wrap items-center my-2">
+        <div class="flex flex-row justify-evenly">
+            <div class="flex flex-col items-center mr-2">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">100</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Value</h1>
+            </div>
+            <div class="flex flex-col items-center">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">-20</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Decay</h1>
+            </div>
         </div>
-        <div class=" box border border-black flex justify-center p-1 w-fit px-2" style="background-color: {color} ;">
-            <h1 class="text-4xl text-white">100</h1>
+    </div>
+    
+    <div class="border border-black w-[300px] p-2 outer my-2 flex flex-col items-center">
+        <h1 class="text-4xl text-white text-style">Event</h1>
+        <div class="w-full">
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <p class="box-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac erat massa.
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="border border-black w-[300px] p-2 outer my-2 flex flex-col items-center">
+        <h1 class="text-4xl text-white text-style">Bounty</h1>
+        <div class="w-full">
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <p class="box-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac erat massa.
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="border border-black w-[300px] p-2 outer my-2">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">Inventory</h1>
+            </div>
+        </div>
+    </div>
+    
+    <div class="border border-black w-[300px] p-2 outer my-2">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">End Turn</h1>
+            </div>
         </div>
     </div>
 </div>
