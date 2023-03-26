5fc3d84e71e5f8650b50af6f53c64ed9fa6c0fb7
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-26 16:59:16 -0400

    Action Page Created

---
+++ src/routes/action/+page.svelte
@@ -0,0 +1,102 @@
+<script>
+    import { onMount } from 'svelte';
+    var color;
+    onMount(() => {
+		color = set_BG();
+	});
+function set_BG(){
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return "#"+randomColor;
+}
+</script>
+
+<div class="flex flex-col justify-center w-full items-center p-4">
+    <div class="border border-black w-[300px] p-2 outer mb-2">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">Action Phase</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="border border-black w-[300px] p-2 outer flex flex-wrap items-center my-2">
+        <div class="flex flex-row justify-evenly">
+            <div class="flex flex-col items-center mr-2">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">100</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Value</h1>
+            </div>
+            <div class="flex flex-col items-center">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">-20</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Decay</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="border border-black w-[300px] p-2 outer flex flex-wrap items-center my-2">
+        <div class="flex flex-row justify-evenly">
+            <div class="flex flex-col items-center mr-2">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">100</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Atk</h1>
+            </div>
+            <div class="flex flex-col items-center">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">-20</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Def</h1>
+            </div>
+        </div>
+
+        <div class="flex flex-row justify-evenly">
+            <div class="flex flex-col items-center mr-2">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">100</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Range</h1>
+            </div>
+            <div class="flex flex-col items-center">
+                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
+                    <h1 class="text-4xl text-white text-style">-20</h1>
+                </div>
+                <h1 class="text-4xl text-white text-style">Mov Range</h1>
+            </div>
+        </div>
+    </div>
+    
+    <div class="border border-black w-[300px] p-2 outer my-2">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">Attack!</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="border border-black w-[300px] p-2 outer my-2">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">Defend!</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="border border-black w-[300px] p-2 outer my-2">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">Act!</h1>
+            </div>
+        </div>
+    </div>
+    
+    <div class="border border-black w-[300px] p-2 outer my-2">
+        <div>
+            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">End Turn</h1>
+            </div>
+        </div>
+    </div>
+</div>
