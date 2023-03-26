6c7bb9b11e69b101365c3c782a9fbc84ab0282c3
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-26 17:28:57 -0400

    Shop progress

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from 'svelte';
+    import { Bomb, Image } from 'lucide-svelte';
     var color;
     onMount(() => {
 		color = set_BG();
@@ -20,58 +21,26 @@ function set_BG(){
     </div>
 
     <div class="border border-black w-[300px] p-2 outer flex flex-col items-center my-2">
-        <div class="flex flex-row justify-evenly">
-            <div class="flex flex-col items-center mr-2">
-                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
-                    <h1 class="text-4xl text-white text-style">100</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Value</h1>
+        <div class="flex flex-row justify-end items-end">
+            <div class="border border-black w-[140px] h-[140px] mx-2">
+
             </div>
-            <div class="flex flex-col items-center">
-                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color: {color} ;">
-                    <h1 class="text-4xl text-white text-style">-20</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Decay</h1>
+            <div class=" box border border-black flex justify-center p-1 w-[125px] h-[50px]" style="background-color: {color} ;">
+                <h1 class="text-4xl text-white text-style">+5</h1>
             </div>
+        </div>
+        <h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
+        <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
+            <p class="box-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac erat massa.
+            </p>
         </div>
     </div>
-
-    <div class="border border-black w-[300px] p-2 outer flex flex-col items-center my-2">
-        <div class="flex flex-row justify-evenly">
-            <div class="flex flex-col items-center mr-2">
-                <div class=" box border border-black flex justify-center p-1 w-[130px]" style="background-color: {color} ;">
-                    <h1 class="text-4xl text-white text-style">+5</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Atk</h1>
-            </div>
-            <div class="flex flex-col items-center">
-                <div class=" box border border-black flex justify-center p-1 w-[130px]" style="background-color: {color} ;">
-                    <h1 class="text-4xl text-white text-style">+10</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Def</h1>
-            </div>
-        </div>
-
-        <div class="flex flex-row justify-evenly">
-            <div class="flex flex-col items-center mr-2">
-                <div class=" box border border-black flex justify-center p-1 w-[130px] " style="background-color: {color} ;">
-                    <h1 class="text-4xl text-white text-style">+4IN</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Range</h1>
-            </div>
-            <div class="flex flex-col items-center">
-                <div class=" box border border-black flex justify-center p-1 w-[130px]" style="background-color: {color} ;">
-                    <h1 class="text-4xl text-white text-style">-3IN</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Mov Range</h1>
-            </div>
-        </div>
-    </div>
-    
+   
     <div class="border border-black w-[300px] p-2 outer my-2">
         <div>
             <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
-                <h1 class="text-4xl text-white text-style">Attack!</h1>
+                <h1 class="text-4xl text-white text-style">Reroll</h1>
             </div>
         </div>
     </div>
