871e5a8c03f1efc01479c4ebc2d36b0ed44c44ad
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-06 11:40:40 -0400

    Added StanceMenu

--- src/lib/StanceMenu.svelte
+++ src/lib/StanceMenu.svelte
@@ -1,23 +1,61 @@
-<div class="border border-black w-[300px] p-2 outer my-3">
-    <div>
-        <div class=" box border border-black flex justify-center p-1 my-2" style="background-color:#CC2C2C ;">
-            <h1 class="text-4xl text-white text-style">Attack</h1>
-        </div>
-    </div>
-    <div>
-        <div class=" box border border-black flex justify-center p-1 my-2" style="background-color:#39ADD1 ;">
-            <h1 class="text-4xl text-white text-style">Defend!</h1>
-        </div>
-    </div>
-    <div>
-        <div class=" box border border-black flex justify-center p-1 my-2" style="background-color:#FFB637 ;">
-            <h1 class="text-4xl text-white text-style">Act!</h1>
-        </div>
-    </div>
+<script lang="ts">
+	import { onMount } from 'svelte';
 
-    <div>
-        <div class=" box border border-black flex justify-center p-1 mt-8" style="background-color:#209525 ;">
-            <h1 class="text-4xl text-white text-style">Confirm Stance</h1>
-        </div>
-    </div>
+	export let state: boolean = false;
+
+	function toggleMenu(): void {
+		state = state ? false : true;
+	}
+
+	let style_hidden: string = 'bg-black top-0 left-0 hidden z-0 w-full h-full';
+	let style_shown: string =
+		'bg-black/50 top-0 left-0 absolute block z-0 w-full h-full flex items-center justify-center';
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class={state ? style_shown : style_hidden}>
+	<div class="border border-black w-[300px] p-4 outer my-3 bg-white">
+		<div on:click={toggleMenu}>
+			<div
+				class=" box border border-black flex  items-center justify-center py-1 px-4 mb-6"
+				style="background-color:#cf142b ;"
+			>
+				<h1 class="text-4xl text-white text-style">Close</h1>
+			</div>
+		</div>
+
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1 my-2"
+				style="background-color:#cf142b ;"
+			>
+				<h1 class="text-4xl text-white text-style">Attack</h1>
+			</div>
+		</div>
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1 my-2"
+				style="background-color:#39ADD1 ;"
+			>
+				<h1 class="text-4xl text-white text-style">Defend!</h1>
+			</div>
+		</div>
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1 my-2"
+				style="background-color:#FFB637 ;"
+			>
+				<h1 class="text-4xl text-white text-style">Act!</h1>
+			</div>
+		</div>
+
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1 mt-8"
+				style="background-color:#209525 ;"
+			>
+				<h1 class="text-4xl text-white text-style">Confirm Stance</h1>
+			</div>
+		</div>
+	</div>
 </div>
--- src/routes/movement/+page.svelte
+++ src/routes/movement/+page.svelte
@@ -1,113 +1,148 @@
-<script>
-    import { onMount } from 'svelte';
-    var color;
-    onMount(() => {
-		color = set_BG();
-	});
-function set_BG(){
-  var randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return "#"+randomColor;
-}
+<script lang="ts">
+	import StanceMenu from '$lib/StanceMenu.svelte';
+	import { onMount } from 'svelte';
+	let change: boolean;
+	function toggleMenu(): void {
+		change = change ? false : true;
+	}
 </script>
 
+<StanceMenu state={change} />
 <div class="flex flex-col justify-center w-full items-center p-4 h-screen">
-    <div class="border border-black w-[300px] p-2 outer mb-2">
-        <div>
-            <div class=" box border border-black flex justify-center p-1" style="background-color:#205295 ;">
-                <h1 class="text-4xl text-white text-style">Movement Phase</h1>
-            </div>
-        </div>
-    </div>
+	<div class="border border-black w-[300px] p-2 outer mb-2">
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1"
+				style="background-color:#205295 ;"
+			>
+				<h1 class="text-4xl text-white text-style">Movement Phase</h1>
+			</div>
+		</div>
+	</div>
 
-    <div class="flex flex-row items-center justify-around w-[300px] my-3">
-        <div class="border border-black w-[140px] p-2 outer">
-            <div>
-                <div class=" box border border-black flex justify-center p-1" style="background-color:#205295 ;">
-                    <h1 class="text-4xl text-white text-style">info</h1>
-                </div>
-            </div>
-        </div>
-        <div class="border border-black w-[140px] p-2 outer">
-            <div>
-                <div class=" box border border-black flex justify-center p-1" style="background-color:#205295 ;">
-                    <h1 class="text-4xl text-white text-style">2011</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <div class="border border-black w-[300px] p-2 outer flex flex-row justify-center items-center my-3">
-        <div class="flex flex-row justify-evenly">
-            <div class="flex flex-col items-center mr-2">
-                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color:#205295 ;">
-                    <h1 class="text-4xl text-white text-style">100</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Value</h1>
-            </div>
-            <div class="flex flex-col items-center">
-                <div class=" box border border-black flex justify-center p-1 w-fit px-10" style="background-color:#205295 ;">
-                    <h1 class="text-4xl text-white text-style">-20</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Decay</h1>
-            </div>
-        </div>
-    </div>
+	<div class="flex flex-row items-center justify-around w-[300px] my-3">
+		<div class="border border-black w-[140px] p-2 outer">
+			<div>
+				<div
+					class=" box border border-black flex justify-center p-1"
+					style="background-color:#205295 ;"
+				>
+					<h1 class="text-4xl text-white text-style">info</h1>
+				</div>
+			</div>
+		</div>
+		<div class="border border-black w-[140px] p-2 outer">
+			<div>
+				<div
+					class=" box border border-black flex justify-center p-1"
+					style="background-color:#205295 ;"
+				>
+					<h1 class="text-4xl text-white text-style">2011</h1>
+				</div>
+			</div>
+		</div>
+	</div>
 
-    <div class="border border-black w-[300px] p-2 outer flex flex-col items-center my-3">
-        <div class="flex flex-row justify-evenly">
-            <div class="flex flex-col items-center mr-2">
-                <div class=" box border border-black flex justify-center p-1 w-[130px]" style="background-color:#CC2C2C ;">
-                    <h1 class="text-4xl text-white text-style">+5</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Atk</h1>
-            </div>
-            <div class="flex flex-col items-center">
-                <div class=" box border border-black flex justify-center p-1 w-[130px]" style="background-color:#39ADD1 ;">
-                    <h1 class="text-4xl text-white text-style">+10</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Def</h1>
-            </div>
-        </div>
+	<div
+		class="border border-black w-[300px] p-2 outer flex flex-row justify-center items-center my-3"
+	>
+		<div class="flex flex-row justify-evenly">
+			<div class="flex flex-col items-center mr-2">
+				<div
+					class=" box border border-black flex justify-center p-1 w-fit px-10"
+					style="background-color:#205295 ;"
+				>
+					<h1 class="text-4xl text-white text-style">100</h1>
+				</div>
+				<h1 class="text-4xl text-white text-style">Value</h1>
+			</div>
+			<div class="flex flex-col items-center">
+				<div
+					class=" box border border-black flex justify-center p-1 w-fit px-10"
+					style="background-color:#205295 ;"
+				>
+					<h1 class="text-4xl text-white text-style">-20</h1>
+				</div>
+				<h1 class="text-4xl text-white text-style">Decay</h1>
+			</div>
+		</div>
+	</div>
 
-        <div class="flex flex-row justify-evenly">
-            <div class="flex flex-col items-center mr-2">
-                <div class=" box border border-black flex justify-center p-1 w-[130px] " style="background-color:#FFB637 ;">
-                    <h1 class="text-4xl text-white text-style">+4IN</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Range</h1>
-            </div>
-            <div class="flex flex-col items-center">
-                <div class=" box border border-black flex justify-center p-1 w-[130px] " style="background-color:#209525 ;">
-                    <h1 class="text-4xl text-white text-style">-3IN</h1>
-                </div>
-                <h1 class="text-4xl text-white text-style">Mov Range</h1>
-            </div>
-        </div>
-    </div>
-    
-    <div class="border border-black w-[300px] p-2 outer my-3">
-        <div>
-            <div class=" box border border-black flex justify-center p-1" style="background-color:#205295 ;">
-                <h1 class="text-4xl text-white text-style">Stance</h1>
-            </div>
-        </div>
-    </div>
+	<div class="border border-black w-[300px] p-2 outer flex flex-col items-center my-3">
+		<div class="flex flex-row justify-evenly">
+			<div class="flex flex-col items-center mr-2">
+				<div
+					class=" box border border-black flex justify-center p-1 w-[130px]"
+					style="background-color:#CC2C2C ;"
+				>
+					<h1 class="text-4xl text-white text-style">+5</h1>
+				</div>
+				<h1 class="text-4xl text-white text-style">Atk</h1>
+			</div>
+			<div class="flex flex-col items-center">
+				<div
+					class=" box border border-black flex justify-center p-1 w-[130px]"
+					style="background-color:#39ADD1 ;"
+				>
+					<h1 class="text-4xl text-white text-style">+10</h1>
+				</div>
+				<h1 class="text-4xl text-white text-style">Def</h1>
+			</div>
+		</div>
 
-    <div class="border border-black w-[300px] p-2 outer my-3">
-        <div>
-            <div class=" box border border-black flex justify-center p-1" style="background-color:#205295 ;">
-                <h1 class="text-4xl text-white text-style">Inventory</h1>
-            </div>
-        </div>
-    </div>
-    
-    <div class="border border-black w-[300px] p-2 outer my-3">
-        <div>
-            <div class=" box border border-black flex justify-center p-1" style="background-color:#205295 ;">
-                <h1 class="text-4xl text-white text-style">End Turn</h1>
-            </div>
-        </div>
-    </div>
+		<div class="flex flex-row justify-evenly">
+			<div class="flex flex-col items-center mr-2">
+				<div
+					class=" box border border-black flex justify-center p-1 w-[130px] "
+					style="background-color:#FFB637 ;"
+				>
+					<h1 class="text-4xl text-white text-style">+4IN</h1>
+				</div>
+				<h1 class="text-4xl text-white text-style">Range</h1>
+			</div>
+			<div class="flex flex-col items-center">
+				<div
+					class=" box border border-black flex justify-center p-1 w-[130px] "
+					style="background-color:#209525 ;"
+				>
+					<h1 class="text-4xl text-white text-style">-3IN</h1>
+				</div>
+				<h1 class="text-4xl text-white text-style">Mov Range</h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="border border-black w-[300px] p-2 outer my-3">
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1"
+				style="background-color:#205295 ;"
+				on:click={toggleMenu}
+			>
+				<h1 class="text-4xl text-white text-style">Stance</h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="border border-black w-[300px] p-2 outer my-3">
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1"
+				style="background-color:#205295 ;"
+			>
+				<h1 class="text-4xl text-white text-style">Inventory</h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="border border-black w-[300px] p-2 outer my-3">
+		<div>
+			<div
+				class=" box border border-black flex justify-center p-1"
+				style="background-color:#205295 ;"
+			>
+				<h1 class="text-4xl text-white text-style">End Turn</h1>
+			</div>
+		</div>
+	</div>
 </div>
-
-
