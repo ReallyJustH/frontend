55a26b543c670902d904f24f9a61e38bd36a0760
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-13 11:48:50 -0400

    Various changes
    - Updated design of Inventory page
    - Updating background of buttons on movement page

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -1,79 +1,83 @@
 <script>
-    import { onMount } from 'svelte';
-    var color;
-    onMount(() => {
-		color = set_BG();
-	});
-function set_BG(){
-  var randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return "#"+randomColor;
-}
 </script>
 
-<div class="flex flex-col justify-center w-full items-center p-4 h-screen">
-    <div class="border border-black w-[300px] p-2 outer mb-2">
-        <div>
-            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
-                <h1 class="text-4xl text-white text-style">Inventory</h1>
-            </div>
-        </div>
-    </div>
+<main class=" bg-gradient-to-b from-[#D9D9D9] to-white">
+	<div class="box z-10">
+		<div class="flex flex-col justify-center w-full items-center p-4 h-screen">
+			<div class="border-2 border-black w-[300px] p-2 outer mb-2 bg-white">
+				<div>
+					<div
+						class=" box border-2 border-black flex justify-center p-1"
+						style="background-color: #D9D9D9 ;"
+					>
+						<h1 class="text-4xl text-white text-style">Inventory</h1>
+					</div>
+				</div>
+			</div>
 
-    <div class="border border-black w-[300px] p-2 outer flex flex-col items-center my-2">
-        <div class="flex flex-row justify-end items-end">
-            <div class="border border-black w-[140px] h-[140px] mx-2">
+			<div
+				class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 bg-white"
+			>
+				<div class="flex flex-row justify-end items-end">
+					<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
+					<div
+						class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+						style="background-color: #CC2C2C ;"
+					>
+						<h1 class="text-4xl text-white text-style">+5</h1>
+					</div>
+				</div>
+				<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
+				<div
+					class=" border-2 border-black flex justify-center p-2"
+					style="background-color: #D9D9D9 ;"
+				>
+					<p class="box-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis
+						nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Curabitur ac erat massa.
+					</p>
+				</div>
+			</div>
 
-            </div>
-            <div class=" box border border-black flex justify-center p-1 w-[125px] h-[50px]" style="background-color: {color} ;">
-                <h1 class="text-4xl text-white text-style">+5</h1>
-            </div>
-        </div>
-        <h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-        <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
-            <p class="box-text text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac erat massa.
-            </p>
-        </div>
-        <div class="flex flex-row w-full items-center justify-between my-2">
-            <h1 class="text-4xl text-white text-style">Uses: 3</h1>
-            <div class=" box border border-black flex justify-center p-1 w-[125px] h-[50px]" style="background-color: {color} ;">
-                <h1 class="text-4xl text-white text-style">Use</h1>
-            </div>
-        </div>
-    </div>
-   
-    <div class="border border-black w-[300px] p-2 outer my-2 flex flex-row items-center justify-evenly">
-        <div class="border border-black w-[45px] h-[45px]">
+			<div
+				class="border-2 border-black w-[300px] p-2 outer my-2 flex flex-row items-center justify-around bg-white"
+			>
+				<div class="border-2 border-black w-[45px] h-[45px]" />
 
-        </div>
-        <div class="flex flex-col mx-1">
-            <h3 class="text-xl text-white text-style">Pebble</h3>
-            <p class="box-text text-black"> Uses: 3 | Atk + 5</p>
-        </div>
-        <div class=" box border border-black flex justify-center p-1 w-[65px] h-[40px] items-center" style="background-color: {color} ;">
-            <h1 class="text-2xl text-white text-style">Use</h1>
-        </div>
-    </div>
+				<p class="box-text text-black">Buff: Atk + 5</p>
 
-    <div class="border border-black w-[300px] p-2 outer my-2 flex items-center justify-evenly">
-            <div class="border border-black w-[45px] h-[45px]">
+				<div
+					class=" box border-2 border-black flex justify-center p-1 w-[65px] h-[40px] items-center"
+					style="background-color: #D9D9D9 ;"
+				>
+					<h1 class="text-2xl text-white text-style">Use</h1>
+				</div>
+			</div>
 
-            </div>
-            <div class="flex flex-col mx-1">
-                <h3 class="text-xl text-white text-style">Pebble</h3>
-                <p class="box-text text-black"> Uses: 3 | Atk + 5</p>
-            </div>
-            <div class=" box border border-black flex justify-center p-1 w-[65px] h-[40px] items-center self-end" style="background-color: {color} ;">
-                <h1 class="text-2xl text-white text-style">Use</h1>
-            </div>
-    </div>
+			<div
+				class="border-2 border-black w-[300px] p-2 outer my-2 flex items-center justify-around bg-white"
+			>
+				<div class="border-2 border-black w-[45px] h-[45px]" />
+				<p class="box-text text-black">Buff: Atk + 5</p>
+				<div
+					class=" box border-2 border-black flex justify-center p-1 w-[65px] h-[40px] items-center self-end"
+					style="background-color: #D9D9D9 ;"
+				>
+					<h1 class="text-2xl text-white text-style">Use</h1>
+				</div>
+			</div>
 
-    
-    <div class="border border-black w-[300px] p-2 outer my-2">
-        <div>
-            <div class=" box border border-black flex justify-center p-1" style="background-color: {color} ;">
-                <h1 class="text-4xl text-white text-style">Return</h1>
-            </div>
-        </div>
-    </div>
-</div>
+			<div class="border-2 border-black w-[300px] p-2 outer my-2 bg-white">
+				<div>
+					<div
+						class=" box border-2 border-black flex justify-center p-1"
+						style="background-color: #205295 ;"
+					>
+						<h1 class="text-4xl text-white text-style">Return</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
