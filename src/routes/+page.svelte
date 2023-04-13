24e900d565c256308d37ed02c13137da6f788afa
jcleme <clemensjosh@protonmail.com>
2023-04-13 19:37:52 -0400

    added basic PlayerCard for landing page overview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import PlayerCard from '$lib/PlayerCard.svelte';
 	import { id, thisPlayer, allPlayers, connection } from '$lib/stores';
 </script>
 
@@ -6,12 +7,13 @@
 	<div class="w-[300px] text-center">
 		<h1 class="text-6xl text-teal-500 text-style">Marketplace Melee!</h1>
 	</div>
-	<div>{$id}</div>
-	{#if $allPlayers?.length > 0}
-		{#each $allPlayers as player}
-			<div>{player.id}</div>
-		{/each}
-	{/if}
+	<div class="flex flex-row flex-wrap justify-between items-center">
+		{#if $allPlayers?.length > 0}
+			{#each $allPlayers as player}
+				<PlayerCard id={player.id} value={player.value} />
+			{/each}
+		{/if}
+	</div>
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<div class="border border-black bg-white w-[300px] p-2 outer my-2" id="join">
 		<div>
