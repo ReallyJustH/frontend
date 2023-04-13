12affa11747e4617c1fff0648459ef11d59ed47b
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-13 11:04:32 -0400

    Shop updates

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,169 +1,173 @@
 <script>
 </script>
 
-<div class="flex flex-col justify-center w-full items-center p-4 h-screen">
-	<div class="border-2 border-black w-[300px] p-2 outer mb-2">
-		<div>
+<main class=" bg-gradient-to-b from-[#631D73] to-white">
+	<div class="box z-10">
+		<div class="flex flex-col justify-center w-full items-center p-4 h-screen z-20">
+			<div class="border-2 border-black w-[300px] p-2 outer mb-2 bg-white">
+				<div>
+					<div
+						class=" box border-2 border-black flex justify-center p-1"
+						style="background-color: #631D73  ;"
+					>
+						<h1 class="text-4xl text-white text-style">Shop</h1>
+					</div>
+				</div>
+			</div>
+
 			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #205295  ;"
+				class=" whitespace-nowrap overflow-y-hidden overflow-x-scroll w-[310px] flex inline-flex"
 			>
-				<h1 class="text-4xl text-white text-style">Shop</h1>
+				<div
+					class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white"
+				>
+					<div class="flex flex-row justify-end items-end">
+						<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
+						<div
+							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+							style="background-color: #CC2C2C  ;"
+						>
+							<h1 class="text-4xl text-white text-style">+5</h1>
+						</div>
+					</div>
+					<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
+					<div
+						class=" border-2 border-black flex justify-center p-1"
+						style="background-color: #D9D9D9  ;"
+					>
+						<p class="box-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis
+							nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Curabitur ac erat massa.
+						</p>
+					</div>
+					<div class="flex flex-row w-full items-center justify-between my-2">
+						<h1 class="text-4xl text-white text-style">Cost: 15</h1>
+						<div
+							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+							style="background-color: #631D73 ;"
+						>
+							<h1 class="text-4xl text-white text-style">Buy</h1>
+						</div>
+					</div>
+				</div>
+
+				<div
+					class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white"
+				>
+					<div class="flex flex-row justify-end items-end">
+						<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
+						<div
+							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+							style="background-color: #CC2C2C  ;"
+						>
+							<h1 class="text-4xl text-white text-style">+5</h1>
+						</div>
+					</div>
+					<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
+					<div
+						class="border-2 border-black flex justify-center p-1"
+						style="background-color: #D9D9D9  ;"
+					>
+						<p class="box-text ">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis
+							nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Curabitur ac erat massa.
+						</p>
+					</div>
+					<div class="flex flex-row w-full items-center justify-between my-2">
+						<h1 class="text-4xl text-white text-style">Cost: 15</h1>
+						<div
+							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+							style="background-color: #631D73 ;"
+						>
+							<h1 class="text-4xl text-white text-style">Buy</h1>
+						</div>
+					</div>
+				</div>
+
+				<div
+					class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white"
+				>
+					<div class="flex flex-row justify-end items-end">
+						<div class="border border-black w-[140px] h-[140px] mx-2" />
+						<div
+							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+							style="background-color: #CC2C2C  ;"
+						>
+							<h1 class="text-4xl text-white text-style">+5</h1>
+						</div>
+					</div>
+					<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
+					<div
+						class="border-2 border-black flex justify-center p-1"
+						style="background-color: #D9D9D9  ;"
+					>
+						<p class="box-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis
+							nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Curabitur ac erat massa.
+						</p>
+					</div>
+					<div class="flex flex-row w-full items-center justify-between my-2">
+						<h1 class="text-4xl text-white text-style">Cost: 15</h1>
+						<div
+							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+							style="background-color: #631D73 ;"
+						>
+							<h1 class="text-4xl text-white text-style">Buy</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="border-2 border-black w-[300px] p-2 outer my-2 bg-white">
+				<div>
+					<div
+						class=" box border-2 border-black flex justify-center p-1"
+						style="background-color: #631D73  ;"
+					>
+						<h1 class="text-4xl text-white text-style">Reroll</h1>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="border-2 border-black w-[300px] p-2 outer my-2 flex flex-row items-center bg-white"
+			>
+				<div class="border-2 border-black w-[45px] h-[45px] mx-2" />
+				<p class="box-text text-black mr-2">Cost: 5 Value | Atk + 5</p>
+				<div
+					class=" box border-2 border-black flex justify-center p-1 w-[65px] h-[40px] items-center"
+					style="background-color: #D9D9D9  ;"
+				>
+					<h1 class="text-3xl text-white text-style">Buy</h1>
+				</div>
+			</div>
+
+			<div
+				class="border-2 border-black w-[300px] p-2 outer my-2 flex flex-row items-center bg-white"
+			>
+				<div class="border-2 border-black w-[45px] h-[45px] mx-2" />
+				<p class="box-text text-black mr-2">Cost: 5 Value | Atk + 5</p>
+				<div
+					class=" box border-2 border-black flex justify-center p-1 w-[65px] h-[40px] items-center"
+					style="background-color: #D9D9D9  ;"
+				>
+					<h1 class="text-3xl text-white text-style">Buy</h1>
+				</div>
+			</div>
+
+			<div class="border-2 border-black w-[300px] p-2 outer my-2 bg-white">
+				<div>
+					<div
+						class=" box border-2 border-black flex justify-center p-1"
+						style="background-color: #205295  ;"
+					>
+						<h1 class="text-4xl text-white text-style">End Turn</h1>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-
-	<div class=" whitespace-nowrap overflow-y-hidden overflow-x-scroll w-[310px] flex inline-flex">
-		<div
-			class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal"
-		>
-			<div class="flex flex-row justify-end items-end">
-				<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
-				<div
-					class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-					style="background-color: #205295  ;"
-				>
-					<h1 class="text-4xl text-white text-style">+5</h1>
-				</div>
-			</div>
-			<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #205295  ;"
-			>
-				<p class="box-text text-white">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla
-					condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Curabitur ac erat massa.
-				</p>
-			</div>
-			<div class="flex flex-row w-full items-center justify-between my-2">
-				<h1 class="text-4xl text-white text-style">Cost: 15</h1>
-				<div
-					class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-					style="background-color: #205295 ;"
-				>
-					<h1 class="text-4xl text-white text-style">Buy</h1>
-				</div>
-			</div>
-		</div>
-
-		<div
-			class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal"
-		>
-			<div class="flex flex-row justify-end items-end">
-				<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
-				<div
-					class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-					style="background-color: #205295  ;"
-				>
-					<h1 class="text-4xl text-white text-style">+5</h1>
-				</div>
-			</div>
-			<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #205295  ;"
-			>
-				<p class="box-text text-white">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla
-					condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Curabitur ac erat massa.
-				</p>
-			</div>
-			<div class="flex flex-row w-full items-center justify-between my-2">
-				<h1 class="text-4xl text-white text-style">Cost: 15</h1>
-				<div
-					class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-					style="background-color: #205295 ;"
-				>
-					<h1 class="text-4xl text-white text-style">Buy</h1>
-				</div>
-			</div>
-		</div>
-
-		<div
-			class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal"
-		>
-			<div class="flex flex-row justify-end items-end">
-				<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
-				<div
-					class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-					style="background-color: #205295  ;"
-				>
-					<h1 class="text-4xl text-white text-style">+5</h1>
-				</div>
-			</div>
-			<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #205295  ;"
-			>
-				<p class="box-text text-white">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla
-					condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Curabitur ac erat massa.
-				</p>
-			</div>
-			<div class="flex flex-row w-full items-center justify-between my-2">
-				<h1 class="text-4xl text-white text-style">Cost: 15</h1>
-				<div
-					class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-					style="background-color: #205295 ;"
-				>
-					<h1 class="text-4xl text-white text-style">Buy</h1>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="border-2 border-black w-[300px] p-2 outer my-2">
-		<div>
-			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #205295  ;"
-			>
-				<h1 class="text-4xl text-white text-style">Reroll</h1>
-			</div>
-		</div>
-	</div>
-
-	<div class="border-2 border-black w-[300px] p-2 outer my-2 flex flex-row items-center">
-		<div class="border-2 border-black w-[45px] h-[45px] mx-2" />
-		<div class="flex flex-col mx-1">
-			<h3 class="text-xl text-white text-style">Pebble</h3>
-			<p class="box-text text-black">Cost: 5 Value | Atk + 5</p>
-		</div>
-		<div
-			class=" box border-2 border-black flex justify-center p-1 w-[65px] h-[40px] items-center"
-			style="background-color: #205295  ;"
-		>
-			<h1 class="text-2xl text-white text-style">Buy</h1>
-		</div>
-	</div>
-
-	<div class="border-2 border-black w-[300px] p-2 outer my-2 flex flex-row items-center">
-		<div class="border-2 border-black w-[45px] h-[45px] mx-2" />
-		<div class="flex flex-col mx-1">
-			<h3 class="text-xl text-white text-style">Pebble</h3>
-			<p class="box-text text-black">Cost: 5 Value | Atk + 5</p>
-		</div>
-		<div
-			class=" box border-2 border-black flex justify-center p-1 w-[65px] h-[40px] items-center"
-			style="background-color: #205295  ;"
-		>
-			<h1 class="text-2xl text-white text-style">Buy</h1>
-		</div>
-	</div>
-
-	<div class="border-2 border-black w-[300px] p-2 outer my-2">
-		<div>
-			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #205295  ;"
-			>
-				<h1 class="text-4xl text-white text-style">End Turn</h1>
-			</div>
-		</div>
-	</div>
-</div>
+</main>
