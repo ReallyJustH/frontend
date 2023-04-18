941f175115c3aa4eeaa7f95e26df41eb6c0820f4
jcleme <clemensjosh@protonmail.com>
2023-04-18 11:41:26 -0400

    fix admin command protocol

--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -6,7 +6,7 @@
 
 <button
 	class="p-4 border-2 border-red-600 bg-slate-500 text-white rounded-lg m-4"
-	on:click={() => SendMessage($connection, 'CLIENT$$anyClient$$resetGame')}>reset game</button
+	on:click={() => SendMessage($connection, 'ADMIN$$noClient$$resetGame')}>reset game</button
 >
 
 <div class="m-2 p-4 border border-black flex flex-col gap-4 justify-center items-center">
