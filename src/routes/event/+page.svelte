b4152c5ba9a6ea0de7952cd08e84568d7f4f29c3
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-19 08:45:57 -0400

    Updated event page

--- src/routes/event/+page.svelte
+++ src/routes/event/+page.svelte
@@ -5,6 +5,27 @@
 	import { goto } from '$app/navigation';
 </script>
 
-<div>a fun and historically informative event</div>
+<main class=" bg-gradient-to-b from-[#205295] to-white">
+	<div class="box z-10">
+		<div class="border-2 border-black w-[300px] p-2 outer mb-2 bg-white">
+			<div
+				class=" box border-2 border-black flex justify-center p-1"
+				style="background-color: #D9D9D9  ;"
+			>
+				<p class="box-text">A fun and historically informative event.</p>
+			</div>
+		</div>
 
-<button class="text-4xl text-white text-style" on:click={() => goto(`${base}/shop`)}> Done </button>
+		<button
+			class="border-2 border-black w-[300px] p-2 outer my-3 bg-white"
+			on:click={() => goto(`${base}/shop`)}
+		>
+			<div
+				class=" box border-2 border-black flex justify-center p-1"
+				style="background-color: #D9D9D9 ;"
+			>
+				<h1 class="text-4xl text-white text-style">Done</h1>
+			</div>
+		</button>
+	</div>
+</main>
