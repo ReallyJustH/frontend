5e6bdb915f6447713705798a92d5a9347f810cc5
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-19 09:00:09 -0400

    Updated Player Card Styling

--- src/lib/PlayerCard.svelte
+++ src/lib/PlayerCard.svelte
@@ -3,7 +3,11 @@
 	export let value: number;
 </script>
 
-<div class="border-2 border-black rounded-lg p-4 m-2">
-	<h2>{id}</h2>
-	<h2>{value}</h2>
+<div class="border-2 border-black w-[300px] p-2 outer my-2 bg-white">
+	<div
+		class=" box border-2 border-black flex justify-center p-1"
+		style="background-color:#205295 ;"
+	>
+		<h1 class="text-4xl text-white text-style">{id} | {value}</h1>
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,32 +4,36 @@
 	import { SendMessage } from '$lib/utils';
 </script>
 
-<div class="flex flex-col justify-center w-full items-center p-4 my-2" style="">
-	<div class="w-[300px] text-center">
-		<h1 class="text-6xl text-teal-500 text-style">Marketplace Melee!</h1>
-	</div>
-	<div class="flex flex-row flex-wrap justify-between items-center">
-		{#if $allPlayers?.length > 0}
-			{#each $allPlayers as player}
-				<PlayerCard id={player.id} value={player.value} />
-			{/each}
-		{/if}
-	</div>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<button class="border border-black bg-white w-[300px] p-2 outer my-2" id="join">
-		<div
-			class=" box border border-black flex justify-center p-1"
-			style="background-color: #205295;"
-		>
-			<h1
-				class="text-4xl text-white text-style"
-				on:click={() => {
-					SendMessage($connection, 'CLIENT$$' + $id + '$$ready');
-					SendMessage($connection, 'CLIENT$$' + $id + '$$startGame');
-				}}
-			>
-				Start Game
-			</h1>
+<main class=" bg-gradient-to-b from-[#205295] to-white">
+	<div class="box z-10">
+		<div class="flex flex-col justify-center w-full items-center p-4 my-2 h-screen" style="">
+			<div class="w-[300px] text-center">
+				<h1 class="text-6xl text-white text-style">Marketplace Melee!</h1>
+			</div>
+			<div class="flex flex-col flex-wrap justify-between items-center my-2">
+				{#if $allPlayers?.length > 0}
+					{#each $allPlayers as player}
+						<PlayerCard id={player.id} value={player.value} />
+					{/each}
+				{/if}
+			</div>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<button class="border border-black bg-white w-[300px] p-2 outer my-2" id="join">
+				<div
+					class=" box border border-black flex justify-center p-1"
+					style="background-color: #205295;"
+				>
+					<h1
+						class="text-4xl text-white text-style"
+						on:click={() => {
+							SendMessage($connection, 'CLIENT$$' + $id + '$$ready');
+							SendMessage($connection, 'CLIENT$$' + $id + '$$startGame');
+						}}
+					>
+						Start Game
+					</h1>
+				</div>
+			</button>
 		</div>
-	</button>
-</div>
+	</div>
+</main>
