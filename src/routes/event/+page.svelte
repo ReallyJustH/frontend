9888e802211998b616eaa0f10db5cdd07ddc2e74
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-19 08:54:37 -0400

    Styled Event Page

--- src/routes/event/+page.svelte
+++ src/routes/event/+page.svelte
@@ -7,25 +7,35 @@
 
 <main class=" bg-gradient-to-b from-[#205295] to-white">
 	<div class="box z-10">
-		<div class="border-2 border-black w-[300px] p-2 outer mb-2 bg-white">
-			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #D9D9D9  ;"
-			>
-				<p class="box-text">A fun and historically informative event.</p>
+		<div class="flex flex-col justify-center w-full items-center p-4 h-screen">
+			<div class="border-2 border-black w-[300px] p-2 outer mb-6 bg-white">
+				<div
+					class=" box border-2 border-black flex justify-center p-1 text-center"
+					style="background-color: #205295 ;"
+				>
+					<p class="text-style text-white text-4xl">Event</p>
+				</div>
 			</div>
-		</div>
+			<div class="border-2 border-black w-[300px] p-2 outer mb-2 bg-white">
+				<div
+					class=" box border-2 border-black flex justify-center p-1 text-center"
+					style="background-color: #D9D9D9  ;"
+				>
+					<p class="text-style text-white text-3xl">A fun and historically informative event.</p>
+				</div>
+			</div>
 
-		<button
-			class="border-2 border-black w-[300px] p-2 outer my-3 bg-white"
-			on:click={() => goto(`${base}/shop`)}
-		>
-			<div
-				class=" box border-2 border-black flex justify-center p-1"
-				style="background-color: #D9D9D9 ;"
+			<button
+				class="border-2 border-black w-[300px] p-2 outer my-3 bg-white"
+				on:click={() => goto(`${base}/shop`)}
 			>
-				<h1 class="text-4xl text-white text-style">Done</h1>
-			</div>
-		</button>
+				<div
+					class=" box border-2 border-black flex justify-center p-1"
+					style="background-color: #D9D9D9 ;"
+				>
+					<h1 class="text-4xl text-white text-style">Done</h1>
+				</div>
+			</button>
+		</div>
 	</div>
 </main>
