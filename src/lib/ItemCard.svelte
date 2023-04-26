94df2a48460b730df267b1cfb805b53b5eeea256
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-26 14:12:46 -0400

    Created ItemCard

---
+++ src/lib/ItemCard.svelte
@@ -0,0 +1,30 @@
+<div
+	class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white"
+>
+	<div class="flex flex-row justify-end items-end snap-x">
+		<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
+		<div
+			class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+			style="background-color: #CC2C2C  ;"
+		>
+			<h1 class="text-4xl text-white text-style">+5</h1>
+		</div>
+	</div>
+	<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
+	<div class=" border-2 border-black flex justify-center p-1" style="background-color: #D9D9D9  ;">
+		<p class="box-text">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla
+			condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+			erat massa.
+		</p>
+	</div>
+	<div class="flex flex-row w-full items-center justify-between my-2">
+		<h1 class="text-4xl text-white text-style">Cost: 15</h1>
+		<div
+			class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
+			style="background-color: #631D73 ;"
+		>
+			<h1 class="text-4xl text-white text-style">Buy</h1>
+		</div>
+	</div>
+</div>
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -2,6 +2,7 @@
 	import { SendMessage } from '$lib/utils';
 	import { id, thisPlayer, allPlayers, connection } from '$lib/stores';
 	import LoadingPopUp from '$lib/LoadingPopUp.svelte';
+	import ItemCard from '$lib/ItemCard.svelte';
 
 	/** the name of the item being purchased */
 	let itemName: string;
@@ -28,107 +29,9 @@
 			<div
 				class=" whitespace-nowrap overflow-y-hidden overflow-x-scroll w-[330px] inline-flex scrollbar-hide relative translate-x-[15px]"
 			>
-				<div
-					class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white"
-				>
-					<div class="flex flex-row justify-end items-end snap-x">
-						<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
-						<div
-							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-							style="background-color: #CC2C2C  ;"
-						>
-							<h1 class="text-4xl text-white text-style">+5</h1>
-						</div>
-					</div>
-					<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-					<div
-						class=" border-2 border-black flex justify-center p-1"
-						style="background-color: #D9D9D9  ;"
-					>
-						<p class="box-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis
-							nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Curabitur ac erat massa.
-						</p>
-					</div>
-					<div class="flex flex-row w-full items-center justify-between my-2">
-						<h1 class="text-4xl text-white text-style">Cost: 15</h1>
-						<div
-							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-							style="background-color: #631D73 ;"
-						>
-							<h1 class="text-4xl text-white text-style">Buy</h1>
-						</div>
-					</div>
-				</div>
-
-				<div
-					class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white snap-x"
-				>
-					<div class="flex flex-row justify-end items-end">
-						<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
-						<div
-							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-							style="background-color: #CC2C2C  ;"
-						>
-							<h1 class="text-4xl text-white text-style">+5</h1>
-						</div>
-					</div>
-					<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-					<div
-						class="border-2 border-black flex justify-center p-1"
-						style="background-color: #D9D9D9  ;"
-					>
-						<p class="box-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis
-							nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Curabitur ac erat massa.
-						</p>
-					</div>
-					<div class="flex flex-row w-full items-center justify-between my-2">
-						<h1 class="text-4xl text-white text-style">Cost: 15</h1>
-						<div
-							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-							style="background-color: #631D73 ;"
-						>
-							<h1 class="text-4xl text-white text-style">Buy</h1>
-						</div>
-					</div>
-				</div>
-
-				<div
-					class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white snap-x"
-				>
-					<div class="flex flex-row justify-end items-end">
-						<div class="border border-black w-[140px] h-[140px] mx-2" />
-						<div
-							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-							style="background-color: #CC2C2C  ;"
-						>
-							<h1 class="text-4xl text-white text-style">+5</h1>
-						</div>
-					</div>
-					<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-					<div
-						class="border-2 border-black flex justify-center p-1"
-						style="background-color: #D9D9D9  ;"
-					>
-						<p class="box-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis
-							nulla condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Curabitur ac erat massa.
-						</p>
-					</div>
-					<div class="flex flex-row w-full items-center justify-between my-2">
-						<h1 class="text-4xl text-white text-style">Cost: 15</h1>
-						<div
-							class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-							style="background-color: #631D73 ;"
-						>
-							<h1 class="text-4xl text-white text-style">Buy</h1>
-						</div>
-					</div>
-				</div>
+				<ItemCard />
+				<ItemCard />
+				<ItemCard />
 			</div>
 
 			<button class="border-2 border-black w-[300px] p-2 outer my-2 bg-white">
