46de9f3f14b5f62dff12ee4a410d4275fd490a58
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-04-26 16:28:43 -0400

    Update ItemCard.svelte

--- src/lib/ItemCard.svelte
+++ src/lib/ItemCard.svelte
@@ -13,40 +13,45 @@
 </script>
 
 <div
-	class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white"
+	class="border-2 border-black w-[300px] p-2 outer flex flex-col my-2 mr-4 whitespace-normal bg-white"
 >
+	<h1 class="text-2xl text-white text-style self-start py-1">{itemRarity}</h1>
 	<div class="flex flex-row justify-end items-end snap-x">
-		<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
+		<div class="border-2 border-black w-[140px] h-[140px] mr-2" />
 		<div
 			class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
 			style="background-color: #CC2C2C;"
 		>
-			<h1 class="text-4xl text-white text-style">{itemEffect?.amount}</h1>
+			<h1 class="text-3xl text-white text-style">{itemEffect?.amount}</h1>
 		</div>
 	</div>
-	<h1 class="text-4xl text-white text-style self-start py-1">{itemName}</h1>
-	<div class=" border-2 border-black flex justify-center p-1" style="background-color: #D9D9D9;">
+	<h1 class="text-2xl text-white text-style self-start py-1">{itemName}</h1>
+	<div class=" border-2 border-black flex p-1" style="background-color: #D9D9D9;">
 		<p class="box-text">
 			{itemDescription}
 		</p>
 	</div>
-	<div class="flex flex-row w-full items-center justify-between my-2">
-		<h1 class="text-4xl text-white text-style">Cost: {itemPrice}</h1>
-		<div
-			class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-			style="background-color: #631D73 ;"
-		>
-			<h1
-				class="text-4xl text-white text-style"
-				on:click={() => {
-					SendMessage($connection, 'CLIENT$$' + $id + '$$shop$$' + itemName);
-				}}
-				on:keypress={() => {
-					SendMessage($connection, 'CLIENT$$' + $id + '$$requestNewItems');
-				}}
-			>
-				Buy
-			</h1>
-		</div>
+	<div class="flex flex-row w-full items-center justify-between my-1">
+		<h1 class="text-2xl text-white text-style">Cost: {itemPrice}</h1>
 	</div>
+	<div class="flex flex-row w-full items-center justify-between my-1">
+		<h1 class="text-2xl text-white text-style">Uses: {numUses}</h1>
+	</div>
+
+	<button
+		class=" box border-2 border-black flex justify-center p-1 w-full h-[50px]"
+		style="background-color: #631D73 ;"
+	>
+		<h1
+			class="text-4xl text-white text-style"
+			on:click={() => {
+				SendMessage($connection, 'CLIENT$$' + $id + '$$shop$$' + itemName);
+			}}
+			on:keypress={() => {
+				SendMessage($connection, 'CLIENT$$' + $id + '$$requestNewItems');
+			}}
+		>
+			Buy
+		</h1>
+	</button>
 </div>
