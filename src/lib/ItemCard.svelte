81f55e33570e7a3ee828368cf9f317a66c2e5b34
jcleme <clemensjosh@protonmail.com>
2023-04-26 14:35:58 -0400

    item templating

--- src/lib/ItemCard.svelte
+++ src/lib/ItemCard.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+	import { SendMessage } from '$lib/utils';
+	import { id, thisPlayer, allPlayers, connection } from '$lib/stores';
+	import type { Rarity, Effect } from './types';
+
+	export let itemName: string;
+	export let itemDescription: string;
+	export let itemPrice: string;
+	export let isConsumable: boolean;
+	export let numUses: number = 1;
+	export let itemRarity: Rarity;
+	export let itemEffect: Effect;
+</script>
+
 <div
 	class="border-2 border-black w-[300px] p-2 outer flex flex-col items-center my-2 mr-4 whitespace-normal bg-white"
 >
@@ -5,26 +19,34 @@
 		<div class="border-2 border-black w-[140px] h-[140px] mx-2" />
 		<div
 			class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
-			style="background-color: #CC2C2C  ;"
+			style="background-color: #CC2C2C;"
 		>
-			<h1 class="text-4xl text-white text-style">+5</h1>
+			<h1 class="text-4xl text-white text-style">{itemEffect?.amount}</h1>
 		</div>
 	</div>
-	<h1 class="text-4xl text-white text-style self-start py-1">Pointy Stick</h1>
-	<div class=" border-2 border-black flex justify-center p-1" style="background-color: #D9D9D9  ;">
+	<h1 class="text-4xl text-white text-style self-start py-1">{itemName}</h1>
+	<div class=" border-2 border-black flex justify-center p-1" style="background-color: #D9D9D9;">
 		<p class="box-text">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus ipsum quis nulla
-			condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
-			erat massa.
+			{itemDescription}
 		</p>
 	</div>
 	<div class="flex flex-row w-full items-center justify-between my-2">
-		<h1 class="text-4xl text-white text-style">Cost: 15</h1>
+		<h1 class="text-4xl text-white text-style">Cost: {itemPrice}</h1>
 		<div
 			class=" box border-2 border-black flex justify-center p-1 w-[125px] h-[50px]"
 			style="background-color: #631D73 ;"
 		>
-			<h1 class="text-4xl text-white text-style">Buy</h1>
+			<h1
+				class="text-4xl text-white text-style"
+				on:click={() => {
+					SendMessage($connection, 'CLIENT$$' + $id + '$$shop$$' + itemName);
+				}}
+				on:keypress={() => {
+					SendMessage($connection, 'CLIENT$$' + $id + '$$requestNewItems');
+				}}
+			>
+				Buy
+			</h1>
 		</div>
 	</div>
 </div>
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,9 +1,11 @@
 import { writable } from 'svelte/store';
 import type { Player } from './types';
+import type Item from './types';
 
 export const id = writable<string>();
 export const thisPlayer = writable<Player>();
 export const allPlayers = writable<Player[]>();
+export const shopItems = writable<Item[]>();
 export const deadPlayers = writable<Player[]>();
 export const connection = writable<WebSocket>();
 export let targetPlayer = writable<Player>();
